899d89f795cd957685d8f39969a545d726e8ec31
Asus <Asus@DESKTOP-LLJFP22>
2016-12-12 12:18:27 +0100

    I don't know of what are you talking about

--- src/fr/badblock/survival/listeners/MoveListener.java
+++ src/fr/badblock/survival/listeners/MoveListener.java
@@ -1,11 +1,15 @@
 package fr.badblock.survival.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import fr.badblock.gameapi.BadListener;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.players.BadblockPlayer.BadblockMode;
 import fr.badblock.survival.PluginSurvival;
 import fr.badblock.survival.runnables.game.NoMoveRunnable;
 
@@ -28,6 +32,15 @@ public class MoveListener extends BadListener {
 
 			if(vehicle != null)
 				vehicle.setPassenger(e.getPlayer());
+		}else if(e.getTo().getY() <= 0.0d && inGame()){
+			BadblockPlayer player = (BadblockPlayer) e.getPlayer();
+			if (player.getBadblockMode().equals(BadblockMode.PLAYER)) {
+				@SuppressWarnings("deprecation")
+				EntityDamageEvent event = new EntityDamageEvent(player, EntityDamageEvent.DamageCause.VOID, player.getHealth());
+				player.setLastDamageCause(event);
+				Bukkit.getServer().getPluginManager().callEvent(event);
+				player.setHealth(0.0D);
+			}
 		}
 
 		if(NoMoveRunnable.noMove){
