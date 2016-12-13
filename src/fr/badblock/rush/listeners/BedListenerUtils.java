f96e49d47e0a5c2dba801fa598cbcfe15faa8d31
Asus <Asus@DESKTOP-LLJFP22>
2016-12-13 19:11:59 +0100

    bande de communistes

--- src/fr/badblock/rush/listeners/BedListenerUtils.java
+++ src/fr/badblock/rush/listeners/BedListenerUtils.java
@@ -48,6 +48,8 @@ public class BedListenerUtils {
 					player.sendTranslatedTitle("rush.yourebed");
 			} else {
 				
+				team.die();
+				
 				Block other = team.teamData(RushTeamData.class).findOtherBedPart().getBlock();
 				
 				if(other.equals(block))
--- src/fr/badblock/rush/listeners/FakeEntityInteractListener.java
+++ src/fr/badblock/rush/listeners/FakeEntityInteractListener.java
@@ -9,17 +9,23 @@ import fr.badblock.gameapi.packets.in.play.PlayInUseEntity.UseEntityAction;
 import fr.badblock.rush.entities.RushTeamData;
 
 public class FakeEntityInteractListener extends BadListener {
-	@EventHandler(ignoreCancelled=true,priority=EventPriority.HIGHEST)
+	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onFakeInteract(PlayerFakeEntityInteractEvent e){
-		if(!inGame() || e.getPlayer().getTeam() == null)
-			return;
-		
-		if(e.getAction() == UseEntityAction.INTERACT){
-			
-			boolean isInZone = e.getPlayer().getTeam().teamData(RushTeamData.class).getSpawnSelection().isInSelection(e.getEntity().getLocation());
-			
-			if(!isInZone)
-				e.setCancelled(true);
+		try {
+			if(!inGame() || e.getPlayer().getTeam() == null)
+				return;
+
+			if(e.getAction() == UseEntityAction.INTERACT){
+
+				boolean isInZone = e.getPlayer().getTeam().teamData(RushTeamData.class).getSpawnSelection().isInSelection(e.getEntity().getLocation());
+
+				if(!isInZone) {
+					e.setCancelled(true);
+				}
+			}
+		}catch(Exception error) {
+			System.out.println("Error on FakeEntityInteractListener: " + error.getMessage());
+			error.printStackTrace();
 		}
 
 	}
