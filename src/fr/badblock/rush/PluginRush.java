37a271f1420f0984dede64ec20a813ac6aab9daa
Asus <Asus@DESKTOP-LLJFP22>
2016-12-14 11:57:59 +0100

    Max 15min

--- src/fr/badblock/rush/PluginRush.java
+++ src/fr/badblock/rush/PluginRush.java
@@ -31,6 +31,7 @@ import fr.badblock.rush.listeners.MoveListener;
 import fr.badblock.rush.listeners.PartyJoinListener;
 import fr.badblock.rush.listeners.RushMapProtector;
 import fr.badblock.rush.listeners.SheepListener;
+import fr.badblock.rush.runnables.GameRunnable;
 import fr.badblock.rush.runnables.PreStartRunnable;
 import lombok.Getter;
 import lombok.Setter;
@@ -168,4 +169,10 @@ public class PluginRush extends BadblockPlugin {
 		File configFile = new File(getDataFolder(), CONFIG);
 		JsonUtils.save(configFile, configuration, true);
 	}
+	
+	@Override
+	public boolean isJoinable() {
+		return GameRunnable.time >= 900;
+	}
+	
 }
