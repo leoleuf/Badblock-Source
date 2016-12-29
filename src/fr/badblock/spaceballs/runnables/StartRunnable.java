1d3bddee8f163ff54e99e5542b00f0fc6e4077ef
xMalware <xmalware2@gmail.com>
2016-12-29 04:48:21 +0100

    Fix

--- src/fr/badblock/spaceballs/runnables/StartRunnable.java
+++ src/fr/badblock/spaceballs/runnables/StartRunnable.java
@@ -59,10 +59,7 @@ public class StartRunnable extends BukkitRunnable {
 		}
 		
 		sendTimeHidden(time);
-
-		boolean ok = time > 10;
-		time -= TIME_BEFORE_START / Bukkit.getMaxPlayers();
-		if (time < 10 && ok) time = 10;
+		
 		time--;
 	}
 	
@@ -108,6 +105,12 @@ public class StartRunnable extends BukkitRunnable {
 	}
 
 	public static void joinNotify(int currentPlayers, int maxPlayers){
+		if (task != null) {
+			boolean ok = task.time > 10;
+			task.time -= (TIME_BEFORE_START / Bukkit.getMaxPlayers()) * 0.25;
+			if (task.time < 10 && ok) task.time = 10;
+			task.time--;
+		}
 		if(currentPlayers < PluginSB.getInstance().getConfiguration().minPlayers) return;
 		
 		startGame(false);
