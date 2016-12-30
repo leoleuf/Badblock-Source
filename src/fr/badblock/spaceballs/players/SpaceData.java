3e8c37d71c8d380ce5006716fe03c38b184acb14
xMalware <xmalware2@gmail.com>
2016-12-30 04:43:55 +0100

    fix

--- src/fr/badblock/spaceballs/players/SpaceData.java
+++ src/fr/badblock/spaceballs/players/SpaceData.java
@@ -11,24 +11,26 @@ public class SpaceData implements InGameData {
 	public int deaths		= 0;
 	public int diamonds     = 0;
 	public int rockets      = 0;
-	
+
 	boolean bejwelerIncremented = false;
-	
+
 	public int getScore(){
 		return (kills * 20 + diamonds * 5) / (deaths == 0 ? 1 : (/*10 * */deaths));
 	}
-	
+
 	public void launchRocket(BadblockPlayer player){
 		rockets++;
 		player.getPlayerData().incrementAchievements(player, SBAchievementList.SB_ROCKET_1, SBAchievementList.SB_ROCKET_2, SBAchievementList.SB_ROCKET_3, SBAchievementList.SB_ROCKET_4, SBAchievementList.SB_ROCKETER);
 	}
-	
+
 	public void putDiamant(BadblockPlayer player, int count){
 		diamonds += count;
-		
+
 		player.getPlayerData().increaseStatistic("spaceballs", SpaceScoreboard.DIAMONDS, count);
-		player.getPlayerData().incrementAchievements(player, SBAchievementList.SB_JEWELER_1, SBAchievementList.SB_JEWELER_2, SBAchievementList.SB_JEWELER_3, SBAchievementList.SB_JEWELER_4, SBAchievementList.SB_VBJEWELER);
-	
+		for (int i = 0; i <= count; i++) {
+			player.getPlayerData().incrementAchievements(player, SBAchievementList.SB_JEWELER_1, SBAchievementList.SB_JEWELER_2, SBAchievementList.SB_JEWELER_3, SBAchievementList.SB_JEWELER_4, SBAchievementList.SB_VBJEWELER);
+		}
+
 		if(diamonds >= 20 && !bejwelerIncremented){
 			bejwelerIncremented = true;
 			player.getPlayerData().incrementAchievements(player, SBAchievementList.SB_BJEWELER_1, SBAchievementList.SB_BJEWELER_2, SBAchievementList.SB_BJEWELER_3, SBAchievementList.SB_BJEWELER_4);
