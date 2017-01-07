1a9773b4c8c13e44bab63138fe56bed9607f476b
xMalware <xmalware2@gmail.com>
2017-01-07 17:39:57 +0100

    fix

--- src/fr/badblock/rush/runnables/StartRunnable.java
+++ src/fr/badblock/rush/runnables/StartRunnable.java
@@ -131,7 +131,7 @@ public class StartRunnable extends BukkitRunnable {
 	public static void joinNotify(int currentPlayers, int maxPlayers){
 		if (task != null) {
 			int a = task.time - (TIME_BEFORE_START / Bukkit.getMaxPlayers());
-			if ((a < task.time && task.time <= 10) || (a < 10 && task.time >= 10)) task.time = 10;
+			if ((a < task.time && task.time <= 10) || ((a < 10 || Bukkit.getOnlinePlayers().size() >= Bukkit.getMaxPlayers()) && task.time >= 10)) task.time = 10;
 			else task.time = a;
 		}
 		int minPlayers = PluginRush.getInstance().getConfiguration().minPlayers;
