b021181c7ddba5a3ea0733bd31d717cf167d9e89
xMalware <xmalware2@gmail.com>
2017-05-13 23:45:58 +0200

    fix

--- src/fr/badblock/bukkit/games/spaceballs/listeners/QuitListener.java
+++ src/fr/badblock/bukkit/games/spaceballs/listeners/QuitListener.java
@@ -3,15 +3,22 @@ package fr.badblock.bukkit.games.spaceballs.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import fr.badblock.bukkit.games.spaceballs.PluginSB;
+import fr.badblock.bukkit.games.spaceballs.runnables.StartRunnable;
 import fr.badblock.gameapi.BadListener;
 import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.players.BadblockTeam;
+import fr.badblock.gameapi.utils.BukkitUtils;
 import fr.badblock.gameapi.utils.i18n.TranslatableString;
 
 public class QuitListener extends BadListener {
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e){
+		if (StartRunnable.gameTask == null && BukkitUtils.getPlayers().size() - 1 < PluginSB.getInstance().getConfiguration().minPlayers) {
+			StartRunnable.stopGame();
+			StartRunnable.time = 60;
+		}
 		if(!inGame()) return;
 		
 		BadblockPlayer player = (BadblockPlayer) e.getPlayer();
