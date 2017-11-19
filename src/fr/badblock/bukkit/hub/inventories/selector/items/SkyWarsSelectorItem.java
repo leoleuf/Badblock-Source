71e5fc1f34aaae7ee8e73345a2e7e71092e05555
xMalware <xmalware2@gmail.com>
2017-11-19 21:28:22 +0100

    fix

--- src/fr/badblock/bukkit/hub/inventories/selector/items/SkyWarsSelectorItem.java
+++ src/fr/badblock/bukkit/hub/inventories/selector/items/SkyWarsSelectorItem.java
@@ -3,17 +3,15 @@ package fr.badblock.bukkit.hub.inventories.selector.items;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 import fr.badblock.bukkit.hub.BadBlockHub;
 import fr.badblock.bukkit.hub.inventories.abstracts.actions.ItemAction;
-import fr.badblock.bukkit.hub.objects.HubPlayer;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.run.BadblockGame;
-import fr.badblock.gameapi.sentry.SEntry;
-import fr.badblock.rabbitconnector.RabbitPacketType;
-import fr.badblock.utils.Encodage;
+import fr.badblock.gameapi.utils.ConfigUtils;
 
 public class SkyWarsSelectorItem extends GameSelectorItem {
 
@@ -36,8 +34,11 @@ public class SkyWarsSelectorItem extends GameSelectorItem {
 	@Override
 	public void onClick(BadblockPlayer player, ItemAction itemAction, Block clickedBlock)
 	{
-		BadBlockHub hub = BadBlockHub.getInstance();
-		hub.getRabbitService().sendAsyncPacket("networkdocker.sentry.join", hub.getGson().toJson(new SEntry(HubPlayer.getRealName(player), getGamePrefix(), false)), Encodage.UTF8, RabbitPacketType.PUBLISHER, 5000, false);
+		Location location = ConfigUtils.getLocation(BadBlockHub.getInstance(), "skywars");
+		if (location == null)
+			player.sendTranslatedMessage("hub.gameunavailable");
+		else
+			player.teleport(location);
 	}
 
 	@Override
