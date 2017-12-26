561b7a46d1e4cbac917f9521b994216655738155
xMalware <xmalware2@gmail.com>
2017-12-26 01:10:47 +0100

    f

--- src/fr/badblock/bukkit/hub/v1/inventories/market/cosmetics/auracolor/AuraColorInventory.java
+++ src/fr/badblock/bukkit/hub/v1/inventories/market/cosmetics/auracolor/AuraColorInventory.java
@@ -2,6 +2,7 @@ package fr.badblock.bukkit.hub.v1.inventories.market.cosmetics.auracolor;
 
 import fr.badblock.bukkit.hub.v1.inventories.abstracts.inventories.CustomInventory;
 import fr.badblock.bukkit.hub.v1.inventories.market.cosmetics.auracolor.select.AuraColor1SelectorItem;
+import fr.badblock.bukkit.hub.v1.inventories.market.cosmetics.auracolor.select.AuraColor2SelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.market.cosmetics.inventoryitems.BackCosmeticsItem;
 
 public class AuraColorInventory extends CustomInventory
@@ -11,7 +12,7 @@ public class AuraColorInventory extends CustomInventory
 	{
 		super("hub.items.auracolorinventory", 1);
 		this.setItem(2, new AuraColor1SelectorItem());
-		this.setItem(4, new AuraColor1SelectorItem());
+		this.setItem(5, new AuraColor2SelectorItem());
 		this.setItem(8, new BackCosmeticsItem());
 	}
 
