dfb6cd5b09cd5067e78c3b11ce809a33a497256a
xMalware <xmalware2@gmail.com>
2018-03-17 17:52:48 +0100

    Added shootflag

--- src/fr/badblock/bukkit/hub/v1/inventories/selector/SelectorInventory.java
+++ src/fr/badblock/bukkit/hub/v1/inventories/selector/SelectorInventory.java
@@ -13,6 +13,7 @@ import fr.badblock.bukkit.hub.v1.inventories.selector.items.PvPBoxSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.PvPFactionSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.QuitSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.RushSelectorItem;
+import fr.badblock.bukkit.hub.v1.inventories.selector.items.ShootFlagSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.SkyBlockSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.SkyWarsSelectorItem;
 import fr.badblock.bukkit.hub.v1.inventories.selector.items.SpaceBallsSelectorItem;
@@ -34,7 +35,8 @@ public class SelectorInventory extends CustomInventory {
 		this.setItem(0, new SpawnSelectorItem());
 		this.setItem(4, new GoogleAuthSelectorItem());
 		this.setItem(8, new VIPZoneSelectorItem());
-		this.setItem(22, new SkyWarsSelectorItem());
+		this.setItem(22, new ShootFlagSelectorItem());
+		this.setItem(27, new TowerRunSelectorItem());
 		this.setItem(28, new TowerSelectorItem());
 		this.setItem(29, new RushSelectorItem());
 		this.setItem(30, new SpeedUHCSelectorItem());
@@ -42,15 +44,15 @@ public class SelectorInventory extends CustomInventory {
 		this.setItem(32, new SurvivalGamesSelectorItem());
 		this.setItem(33, new SpaceBallsSelectorItem());
 		this.setItem(34, new PearlsWarSelectorItem());
-		this.setItem(37, new TowerRunSelectorItem());
+		this.setItem(35, new BuildContestSelectorItem());
+		this.setItem(37, new SkyWarsSelectorItem());
 		this.setItem(38, new PvPFactionSelectorItem());
 		this.setItem(39, new PvPBoxSelectorItem());
 		this.setItem(40, new SkyBlockSelectorItem());
 		this.setItem(41, new FreeBuildSelectorItem());
 		this.setItem(42, new DayZSelectorItem());
-		this.setItem(43, new BuildContestSelectorItem());
+		this.setItem(43, new PointOutSelectorItem());
 		this.setItem(45, new HubChangerSelectorItem());
-		this.setItem(49, new PointOutSelectorItem());
 		this.setItem(53, new QuitSelectorItem());
 		this.setNoFilledItem(new BlueStainedGlassPaneItem());
 	}
