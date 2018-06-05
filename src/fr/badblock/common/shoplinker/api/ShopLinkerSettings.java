5bdaf963a248b65660c66aaa679e58e8eef49288
xMalware <xmalware2@gmail.com>
2018-06-05 15:25:06 +0200

    Removed final modifiers for settings

--- src/fr/badblock/common/shoplinker/api/ShopLinkerSettings.java
+++ src/fr/badblock/common/shoplinker/api/ShopLinkerSettings.java
@@ -7,11 +7,11 @@ import fr.badblock.api.common.tech.rabbitmq.packet.RabbitPacketType;
 public class ShopLinkerSettings
 {
 	
-	public final static String 				QUEUE_PREFIX 		= "shopLinker.";
-	public final static RabbitPacketEncoder	PACKET_ENCODAGE 	= RabbitPacketEncoder.UTF8;
-	public final static RabbitPacketType  	PACKET_TYPE 		= RabbitPacketType.PUBLISHER;
-	public final static RabbitListenerType  LISTENER_TYPE 		= RabbitListenerType.SUBSCRIBER;
-	public final static long				TTL					= 86400_000 * 365;
-	public final static boolean				DEBUG				= true;
+	public static String 				QUEUE_PREFIX 		= "shopLinker.";
+	public static RabbitPacketEncoder	PACKET_ENCODAGE 	= RabbitPacketEncoder.UTF8;
+	public static RabbitPacketType  	PACKET_TYPE 		= RabbitPacketType.PUBLISHER;
+	public static RabbitListenerType  	LISTENER_TYPE 		= RabbitListenerType.SUBSCRIBER;
+	public static long					TTL					= 86400_000 * 365;
+	public static boolean				DEBUG				= true;
 	
 }
